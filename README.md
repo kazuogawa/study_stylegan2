e13ca25731490fd50381d2df567bbe0bcff54df2
ogawa_kazuhisa <ogawa_kazuhisa@cyberagent.co.jp>
2022-02-05 01:11:05 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# stylegan2勉強
+
+https://nn.labml.ai/gan/stylegan/index.html
---
+++ main.py
@@ -0,0 +1,20 @@
+import math
+from typing import Tuple, Optional, List
+
+import numpy as np
+import torch
+import torch.nn.functional as F
+import torch.utils.data
+from torch import nn
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
